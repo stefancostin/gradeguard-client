3083fff55c31459900993be281229172d718cf6b
Stefan Costin <stefanfcostin@gmail.com>
2019-11-05 06:12:29 +0200

    hide navbar on login page

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'grd-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isHeaderHidden: boolean;
+  routerSubscription: Subscription;
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
   ngOnInit() {
+    // hide HeaderComponent when user is on Login page
+    this.routerSubscription = this.router.events.pipe(
+      filter((event: RouterEvent) => {
+        return event instanceof NavigationEnd;
+      })
+    ).subscribe((event: RouterEvent) => {
+      const url = event ? event.url : null;
+      this.isHeaderHidden = (url === '/login') ? true : false;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 
 }
